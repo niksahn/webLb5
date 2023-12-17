323c0a3871f64e1ab74117c75f6f61c394f286bb
Nikita Sahno <n.sahno@smartway.today>
2023-12-17 21:31:57 +0300

    Moderator

--- src/main/java/com/niksahn/laba5/service/NewsService.java
+++ src/main/java/com/niksahn/laba5/service/NewsService.java
@@ -15,6 +15,7 @@ import java.util.Random;
 import java.util.random.RandomGenerator;
 
 import static com.niksahn.laba5.Constants.image_path;
+import static com.niksahn.laba5.Constants.news_path;
 
 @Service
 public class NewsService {
@@ -40,7 +41,7 @@ public class NewsService {
             news = newsRepository.save(new NewsDto(user_id, name, description));
         } else return OperationRezult.No_Right;
         images.forEach(it -> {
-                    String nameFile = news.getId() + "_" + Random.from(RandomGenerator.getDefault()).nextLong();
+                    String nameFile = news_path+news.getId() + "_" + Random.from(RandomGenerator.getDefault()).nextLong();
                     var file = fileService.setImage(it, nameFile);
                     imagesRepository.save(new ImageNewsDto(file, news.getId()));
                 }
